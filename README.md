c90df42fb62c886893e55ad0103cb24eb01df95d
Sahil Ahluwalia <satrader2000@gmail.com>
2023-05-01 18:49:11 +0530

    bareborn PDF Generation

---
+++ README.md
@@ -0,0 +1,13 @@
+## Ahlu PDF Generator for interal use
+
+### INSTALLATION
+1. Clone the repository by running `git clone PROJECT URL`
+2. Run `npm install` to install all the dependencies
+
+
+### USAGE
+1. Add the excel file to the `data` folder
+2. Run `npm start excel` to start the Scraping from existing excel file
+3. Run `npm start pdf` to start the PDF generation
+4. The generated PDF files will be in the `pdf` folder in their respective folders
+
---
+++ writer.js
@@ -0,0 +1,25 @@
+// Requiring module
+const reader = require('xlsx')
+// Reading our test file
+const file = reader.readFile('./test.xlsx')
+  
+// Sample data set
+let student_data = [{
+    Student:'Nikhil',
+    Age:22,
+    Branch:'ISE',
+    Marks: 70
+},
+{
+    Student:'Amitha',
+    Age:21,
+    Branch:'EC',
+    Marks:80
+}]
+var ws = reader.util.writeFileXLSX["Sheet3"];
+// const ws = reader.utils.writeFileXLSX(student_data)
+  
+reader.utils.book_append_sheet(file,ws,"Sheet3")
+  
+// Writing to our file
+reader.writeFile(file,'./test.xlsx')
